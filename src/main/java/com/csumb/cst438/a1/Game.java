58eba6c58e55c3019268c98e97ccc6d599157cc9
david <david@DESKTOP-GB1N4RB>
2017-03-20 13:35:44 -0700

    initial commit

---
+++ src/main/java/com/csumb/cst438/a1/Game.java
@@ -0,0 +1,139 @@
+package com.csumb.cst438.a1;
+
+import static com.csumb.cst438.a1.MyHttpServer.RESOURCE_DIR;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ * Game contains logic to play hangman game.
+ * Includes code to generate new words,
+ * process user guess, determine win or lose.
+ * @author David
+ * @version 1.0
+ */
+public class Game {
+ 
+    private Random generator;  
+    private int state;  
+	           //  1 means no incorrect guesses, or start of game
+                   //  2 means 1 incorrect guess.
+                   //  3 means 2 incorrect guesses.
+                   //  4 means 3 incorrect guesses.
+                   //  5 means 4 incorrect guesses.
+                   //  6 means 5 incorrect guesses.
+	           //  7 means 6 incorrect guesses, user has lost game
+    
+    private String word;   // the word to be guessed 
+    private String displayWord; // part of the word (if any) to show user
+    private ArrayList<String> wordlist;  // list of words
+    
+    public Game() {
+        word="computer";
+        displayWord=createDisplayWord(word);
+        state=1;
+        wordlist=null;
+        generator = new Random();
+    }
+    
+    public int getState(){
+        return state;
+    }
+    
+    public String getWord(){
+        return word;
+    }
+    
+    public String getDisplayWord(){
+        return displayWord;
+    }
+    
+    public void startNewGame() {
+        state = 1;
+        word = "computer";
+        displayWord = "_ _ _ _ _ _ _ _ ";
+        
+    }
+    
+    /*
+     * process a user guess return page saying game over user win or lost, or
+     * return page with updated game display
+     * return 0 = continue game, good guess
+     *        1 = good guess.  Win game. 
+     *        2 = bad guess.  continue game
+     *        3 = bad guess.  Lost game.
+     */
+    public int playGame(char guess) {
+            boolean correctGuess = updateDisplayWord(guess);
+            if (correctGuess==false) { 
+                state++;
+                if (state==7) {
+                    // user has lost game
+                    return 3;
+                } else {
+                    return 2; // bad guess, continue
+                }
+            } else if ( displayWord.contains("_")) {
+               return 0; // continue game, with good guess
+            } else {
+               return 1; // all characters has been guessed, user has won game.
+            }
+    }
+    /**
+     * update display word to show any occurrences of guess
+     * There is a space character between each letter, and 
+     * any letters remaining to be guessed are displayed as underscore
+     * @param guess
+     * @return true if at least one match, false if guess is incorrect
+     */
+    private boolean updateDisplayWord(char guess){
+        boolean correctGuess = false;
+        for (int i=0; i<word.length(); i++) {
+            if (word.charAt(i)==guess){
+                    displayWord = displayWord.substring(0,2*i)+guess+displayWord.substring(2*i+1);
+                    correctGuess=true;
+                }
+        }
+        return correctGuess;
+        
+    }
+    /**
+     * Create the display version of word with letters replaced by
+     * underscore for letter remaining to be guessed
+     * @param word
+     * @return 
+     */
+    private String createDisplayWord(String word){
+        String display="";
+        for (int i=0; i<word.length(); i++) {
+            display = display+ "_ ";
+        }
+        return display;
+    }
+    
+       /**
+     * generate random word.
+     * 
+     * @return a word from the list
+     */
+    private String randomWord() {
+	try {
+            if (wordlist == null) {
+		wordlist = new ArrayList<String>();
+		// read in word list
+		Scanner infile = new Scanner(new File(RESOURCE_DIR + "wordlist.txt"));
+		while (infile.hasNextLine()) {
+                    wordlist.add(infile.nextLine());
+		}
+		infile.close();
+            }
+            int t = generator.nextInt(wordlist.size());
+            return wordlist.get(t);
+
+        } catch (Exception e) {
+            System.out.println("Error randomWord: reading wordlist. " + e.getMessage());
+            System.exit(0);
+            return null; // to keep compiler happy
+	}
+    }
+}
---
+++ src/main/java/com/csumb/cst438/a1/MyHttpServer.java
@@ -0,0 +1,146 @@
+package com.csumb.cst438.a1;  
+import java.net.*;
+import java.util.*;
+import com.sun.net.httpserver.*;
+import java.io.*;
+
+public class MyHttpServer {
+
+	static final String RESOURCE_DIR = "src/main/resources/";
+	static final int PORT = 8080;
+        static Random generator = new Random();
+
+	public static void main(String[] args) {
+
+		try {
+			HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+			System.out.print("Server started. ");
+			// Display current directory
+			System.out.print("pwd=" + System.getProperty("user.dir"));
+			server.createContext("/", new MyHandler());
+			server.setExecutor(null); // creates a default executor
+			server.start();
+			System.out.println();
+			System.out.println("MyHttp Server running.");
+		} catch (Exception e) {
+			System.out.println("My HttpServer Exception: " + e.getMessage());
+		}
+	}
+
+	public static class MyHandler implements HttpHandler {
+            
+                private Game game = new Game();
+                private String cookie="0";     // used to keep track of current game 
+
+                /**
+                 * handle an HTTP request
+                 * @param t
+                 * @throws IOException 
+                 */
+		public void handle(HttpExchange t) throws IOException {
+			String uri = t.getRequestURI().toString();
+			System.out.println("URI=" + uri);
+			if (uri.endsWith(".gif") || uri.endsWith(".ico")) {
+				// http get request for an image file
+				sendFile(t, uri.substring(1));
+			} else {
+				// come here to play the game
+                                String response = "";
+                                // get cookie value from http request
+                                String requestCookie = t.getRequestHeaders().getFirst("Cookie");
+                                System.out.println("Cookie=" + requestCookie);
+                                // if there is no cookie, or it is "0" or differfent from the current value, then start a new game
+                                if (requestCookie==null || requestCookie.equals("0") || cookie.equals("0") || !requestCookie.equals(cookie)) {
+                                    game.startNewGame();
+                                    cookie=generateCookie();    
+                                    response = "<!DOCTYPE html><html><head><title>MyHttpServer</title></head><body><h2>Hangman</h2>"
+                                            + "<img src=\"" + "h" + game.getState() + ".gif" + "\">"
+                                            + "<h2 style=\"font-family:'Lucida Console', monospace\"> " + game.getDisplayWord() + "</h2>"
+                                            + "<form action=\"/\" method=\"get\"> "
+                                            + "Guess a character <input type=\"text\" name=\"guess\"><br>"
+                                            + "<input type=\"submit\" value=\"Submit\">" + "</form></body></html>";
+                                } else {
+                                    // continue with current game
+                                    char ch = uri.charAt(uri.length()-1);  // letter that user has guessed
+                                    int result = game.playGame(ch);
+                                    switch(result) {
+                                        case 0: // good guess, continue game
+                                            response = "<!DOCTYPE html><html><head><title>MyHttpServer</title></head><body><h2>Hangman</h2>"
+                                            + "<img src=\"" + "h" + game.getState() + ".gif" + "\">"
+                                            + "<h2 style=\"font-family:'Lucida Console', monospace\"> " + game.getDisplayWord() + "</h2>"
+                                            + "<form action=\"/\" method=\"get\"> "
+                                            + "Guess a character <input type=\"text\" name=\"guess\"><br>"
+                                            + "<input type=\"submit\" value=\"Submit\">" + "</form></body></html>";
+                                            break;
+                                        case 1: // good guess, win game
+                                             response = "<!DOCTYPE html><html><head><title>MyHttpServer</title></head><body><h2>Hangman</h2>"
+                                            + "<img src=\"" + "h" + game.getState() + ".gif" + "\">"
+                                            + "<h2 style=\"font-family:'Lucida Console', monospace\"> " + "</h2>"
+                                            + "<h2>Congratulations you win!</h2>" + "</body></html>";
+                                             cookie="0";
+                                             break;
+                                        case 2: // bad guess, continue game
+                                            response = "<!DOCTYPE html><html><head><title>MyHttpServer</title></head><body><h2>Hangman</h2>"
+                                            + "<img src=\"" + "h" + game.getState() + ".gif" + "\">"
+                                            + "<h2 style=\"font-family:'Lucida Console', monospace\"> " + game.getDisplayWord() + "</h2>"
+                                            + "<form action=\"/\" method=\"get\"> "
+                                            + "Guess a character <input type=\"text\" name=\"guess\"><br>"
+                                            + "<input type=\"submit\" value=\"Submit\">" + "</form></body></html>";
+                                            break;
+                                        case 3: // bad guess, lost game
+                                             response = "<!DOCTYPE html><html><head><title>MyHttpServer</title></head><body><h2>Hangman</h2>"
+                                            + "<img src=\"" + "h7.gif" + "\">" + "<h2>You lost!  The word is " + game.getWord() + "</h2>"
+                                            + "</body></html>";
+                                             cookie="0";
+                                             break;
+                                    }
+                                    
+                                }
+                                t.getResponseHeaders().set("Content-Type", "text/html");
+                                if (cookie!=null) 
+                                     t.getResponseHeaders().set("Set-Cookie", cookie);
+                                else 
+                                     t.getResponseHeaders().set("Set-Cookie", "0");
+                                System.out.println("New cookie:" + cookie);
+                                t.sendResponseHeaders(200, response.length());
+                                System.out.println("response=" + response);
+                                OutputStream os = t.getResponseBody();
+                                os.write(response.getBytes());
+                                os.close();
+                         }
+                }
+
+                /*
+                 * send a gif file
+                 */
+                public static void sendFile(HttpExchange t, String filename) {
+
+                        try {
+                                File file = new File(RESOURCE_DIR + filename);
+                                byte[] fileData = new byte[(int) file.length()];
+                                DataInputStream dis = new DataInputStream(new FileInputStream(file));
+                                dis.readFully(fileData);
+                                dis.close();
+
+                                t.getResponseHeaders().set("Content-Type", "image/gif");
+                                t.sendResponseHeaders(200, fileData.length);
+                                OutputStream os = t.getResponseBody();
+                                os.write(fileData);
+                                os.close();
+
+                        } catch (Exception e) {
+                                System.out.println("Error in sendFile:" + filename + " " + e.getMessage());
+                        }
+
+                }
+        
+                /*
+                * generate a random cookie which is a random long integer
+                */
+                 public String generateCookie() {
+                    return Long.toString(generator.nextLong());
+                 }
+                 
+        }  // end of static class MyHandler
+        
+}
